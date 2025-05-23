4d7ba7866f05adc11c2affdf509f0d3fdbab54d4
Şahmeranbey <ardasarsilmaz2007@gmail.com>
2025-05-23 22:52:59 +0300

    Update nitrogen.js

--- pages/api/nitrogen.js
+++ pages/api/nitrogen.js
@@ -1,23 +1,25 @@
 export default async function handler(req, res) {
   const ip = req.query.ip;
+
   if (!ip) {
-    return res.status(400).json({ error: 'IP adresi parametresi eksik.' });
+    return res.status(400).json({ error: 'IP parametresi eksik.' });
   }
 
   try {
     const response = await fetch(https://ipwho.is/${ip}?fields=ip,success,country,region,city,latitude,longitude,isp,asn,proxy,tor,threat, {
       headers: {
-        'Accept': 'application/json',
-      },
+        'Accept': 'application/json'
+      }
     });
+
     const data = await response.json();
 
     if (!data.success) {
-      return res.status(404).json({ error: 'IP adresi bulunamadı.' });
+      return res.status(404).json({ error: 'IP bulunamadı.' });
     }
 
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'IP bilgileri alınırken bir hata oluştu.' });
+    return res.status(500).json({ error: 'IP bilgileri alınırken bir hata oluştu.' });
   }
 }

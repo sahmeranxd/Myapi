3dc205676b3652911a2c4a8805a4e664a05dc610
Şahmeranbey <ardasarsilmaz2007@gmail.com>
2025-05-24 12:19:23 +0300

    Create discord.js

---
+++ pages/api/discord.js
@@ -0,0 +1,10 @@
+{
+  "success": true,
+  "message": "Bot ses kanalına başarıyla katıldı!",
+  "data": {
+    "botAdi": "Test Bot#1234",
+    "sunucuId": "1215778341232181309",
+    "kanalId": "1215778341681106977",
+    "baglanti_zamani": "24 Ocak 2024, 15:30"
+  }
+}

3cc7a603cdbc4e0df6d9793d6f189cecb442e977
Şahmeranbey <ardasarsilmaz2007@gmail.com>
2025-05-24 11:35:02 +0300

    deprem.js

---
+++ pages/api/deprem.js
@@ -0,0 +1,13 @@
+[
+    {
+        "city": "MARMARA",
+        "area": "MARMARA DENIZI",
+        "intensity_ml": "1.9",
+        "depth_km": "17.1",
+        "date": 1741289596,
+        "location": "https://www.google.com/maps?q=40.7220,28.2448",
+        "image": "https://tile.openstreetmap.org/10/592/384.png",
+        "lat": "40.7220",
+        "lng": "28.2448"
+    }
+]
